8bdeeb2e37f3bbadc3de2037884616cec6695c18
Your Name <you@example.com>
2022-06-08 20:48:11 +0700

    LeapYear

---
+++ leapyear.js
@@ -0,0 +1,15 @@
+let a= parseInt(prompt('Enter a year'));
+if(a%4==0) {
+    if (a % 100 == 0) {
+        if (a % 400 == 0) {
+            alert(a + 'is leap');
+        } else {
+            alert(a + 'is not leap');
+        }
+    } else {
+        alert(a + 'is not leap');
+    }
+}
+    else{
+        alert(a+'is not leap');
+    }
